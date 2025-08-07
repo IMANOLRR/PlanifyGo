f5fff08bf8a89d3edf39db2f1fffc51c0caefae1
Imanol Rodriguez <133455650+IMANOLRR@users.noreply.github.com>
2025-08-06 22:35:35 -0400

    Registro de tareas del usuario.
    Registro exitoso de las tareas del usuario con un titulo, una descripción, etiquetas, y prioridad.

---
+++ Gestor de Tareas/Gestor de Tareas/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gestor_de_Tareas.Models
+{
+    public class Task
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string tags { get; set; }
+        public int priority { get; set; }
+        public int userId { get; set; }
+
+        
+    }
+}
