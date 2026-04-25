1a4b50f633e8c3bc7425c47124d2c6a0f8ad26cd
Imanol Rodriguez <imanol_2005@icloud.com>
2026-04-25 18:43:23 -0500

    Basura Eliminada

--- PlanifyGo-main/DataBase/gestortareasdb.sql
+++
@@ -1,9 +0,0 @@
--- Crea la base de datos
-CREATE DATABASE IF NOT EXISTS gestortareasdb;
-
--- Usa la base de datos
-USE gestortareasdb;
-
--- Carga el contenido del archivo SQL exportado
-SOURCE Gestor-de-Tareas\DataBase\gestortareasdb_users.sql;
-SOURCE Gestor-de-Tareas\DataBase\gestortareasdb_tasks.sql;
